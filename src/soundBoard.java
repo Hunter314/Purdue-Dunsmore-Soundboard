f054298544009f966474cff8bed7cb5c8901f59d
Kedar Abhyankar <krabhyankar@gmail.com>
2019-04-14 14:41:57 -0400

    added mp3 playing abilities v1

--- src/soundBoard.java
+++ src/soundBoard.java
@@ -1,5 +1,9 @@
+import javax.print.attribute.standard.Media;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class soundBoard {
 
@@ -19,7 +23,6 @@ public class soundBoard {
         initActionListeners();
 
 
-
     }
 
     private static void addImageToFrame(JFrame jf, String fileName) {
@@ -27,36 +30,57 @@ public class soundBoard {
     }
 
     private static void meowSound() {
-
+        playSound("Sounds/meow.mp3");
     }
 
     private static void barkSound() {
+        playSound("Sounds/bork.mp3");
     }
 
     private static void disposeFrameSound() {
-
+        playSound("Sounds/blululululu.mp3");
     }
 
     private static void smallIntegerSound() {
+        playSound("Sounds/int.mp3");
     }
 
     private static void bigIntegerSound() {
+        playSound("Sounds/INTEGER.mp3");
     }
 
     private static void playSound(String fileName) {
-        /**
-         * ENSURE THAT THE FILE GIVEN IS IN THE SOUNDS FOLDER!
-         */
+        File file = new File(fileName);
+        if (file.exists()) {
+            try {
+                AudioInputStream audioInputStream =
+                        AudioSystem.getAudioInputStream(new File(fileName).getAbsoluteFile());
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                JOptionPane.showMessageDialog(null, "Problem in playing file.",
+                        "FATAL ERROR!",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+
+        } else {
+            JOptionPane.showMessageDialog(null, "FILE DOES NOT EXIST!", "FATAL ERROR!",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     private static void setupFrame(JFrame frame) {
         frame.setResizable(false);
         frame.setLayout(new GridLayout());
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(new Dimension(480, 640));
         frame.setVisible(true);
     }
-    private static void initButtons(){
+
+    private static void initButtons() {
         meow = new JButton("Meow");
         bark = new JButton("Bark");
         disposeFrame = new JButton("Bluhluhluh");
@@ -64,7 +88,7 @@ public class soundBoard {
         bigInteger = new JButton("INTEGER");
     }
 
-    private static void initActionListeners(){
+    private static void initActionListeners() {
         meow.addActionListener(ActionListener -> {
             meowSound();
         });
@@ -86,5 +110,5 @@ public class soundBoard {
         });
     }
 
-    
+
 }
