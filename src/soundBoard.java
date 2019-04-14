3291e2783d07263a5259f9c5915a6f34ac883234
Kedar Abhyankar <krabhyankar@gmail.com>
2019-04-14 14:26:49 -0400

    added background image and setup methods

--- src/soundBoard.java
+++ src/soundBoard.java
@@ -1,48 +1,90 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class soundBoard {
 
 
     private static JFrame frame;
-    private JButton meow;
-    JButton bark;
-    JButton disposeFrame;
-    JButton smallInteger;
-    JButton bigInteger;
+    private static JButton meow;
+    private static JButton bark;
+    private static JButton disposeFrame;
+    private static JButton smallInteger;
+    private static JButton bigInteger;
 
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         frame = new JFrame("Purdue University Professor Dunsmore Soundboard");
-    }
+        addImageToFrame(frame, "bxd.png");
+        setupFrame(frame);
+        initButtons();
+        initActionListeners();
 
-    private void addImageToFrame(String filePath) {
 
-    }
-
-    private void meowSound() {
-        meow = new JButton("Meow");
-    }
-
-    private void barkSound() {
-        bark = new JButton("Bark");
-    }
-
-    private void disposeFrameSound() {
-        disposeFrame = new JButton("Bluhluhluh");
 
     }
 
-    private void smallIntegerSound() {
-        smallInteger = new JButton("int");
+    private static void addImageToFrame(JFrame jf, String fileName) {
+        jf.setContentPane(new JLabel(new ImageIcon(fileName)));
     }
 
-    private void bigIntegerSound() {
-        bigInteger = new JButton("INTEGER");
+    private static void meowSound() {
+
     }
 
-    private void playSound(String fileName) {
+    private static void barkSound() {
+    }
+
+    private static void disposeFrameSound() {
+
+    }
+
+    private static void smallIntegerSound() {
+    }
+
+    private static void bigIntegerSound() {
+    }
+
+    private static void playSound(String fileName) {
         /**
          * ENSURE THAT THE FILE GIVEN IS IN THE SOUNDS FOLDER!
          */
-
     }
+
+    private static void setupFrame(JFrame frame) {
+        frame.setResizable(false);
+        frame.setLayout(new GridLayout());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(new Dimension(480, 640));
+        frame.setVisible(true);
+    }
+    private static void initButtons(){
+        meow = new JButton("Meow");
+        bark = new JButton("Bark");
+        disposeFrame = new JButton("Bluhluhluh");
+        smallInteger = new JButton("int");
+        bigInteger = new JButton("INTEGER");
+    }
+
+    private static void initActionListeners(){
+        meow.addActionListener(ActionListener -> {
+            meowSound();
+        });
+
+        bark.addActionListener(ActionListener -> {
+            barkSound();
+        });
+
+        disposeFrame.addActionListener(ActionListener -> {
+            disposeFrameSound();
+        });
+
+        smallInteger.addActionListener(ActionListener -> {
+            smallIntegerSound();
+        });
+
+        bigInteger.addActionListener(ActionListener -> {
+            bigIntegerSound();
+        });
+    }
+
+    
 }
